84f0ba89cc0e478ea9563573ea3bee40f7d4be60
Harsha <105618132+Harsha-Palla@users.noreply.github.com>
2023-11-26 18:43:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+#@st.cache_resource(allow_output_mutation=True)
+def load_model():
+	model = tf.keras.models.load_model('./Lung_disorder.hdf5')
+	return model
+
+
+def predict_class(image, model):
+
+	image = tf.cast(image, tf.float32)
+	image = tf.image.resize(image, [224,224])
+	image = np.expand_dims(image, axis = 0)
+	prediction = model.predict(image)
+
+	return prediction
+
+
+model = load_model()
+st.title('Lung Disorder Classifier')
+
+file = st.file_uploader("Upload a Scan", type=["jpg", "png","jpeg"])
+
+
+if file is None:
+	st.text('Waiting for upload....')
+
+else:
+	slot = st.empty()
+	slot.text('Running inference....')
+
+	test_image = Image.open(file)
+
+	st.image(test_image, caption="Input Image", width = 400)
+
+	pred = predict_class(np.asarray(test_image), model)
+
+	class_names = [ 'Bacterial Pneumonia', 'Corona Virus Disease',  'Normal', 'Tuberculosis', 'Viral Pneumonia']
+
+	result = class_names[np.argmax(pred)]
+
+	output = 'The image is a ' + result
+
+	slot.text('Done')
+
+	st.success(output)
